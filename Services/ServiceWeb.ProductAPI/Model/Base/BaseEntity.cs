e18f6aded7ce00c461547393383c676c90435760
Cleidson Estevam <cleidsonestevamdasilva@hotmail.com>
2023-04-14 21:51:57 -0300

    Adicionado modelo da classe Product.

---
+++ Services/ServiceWeb.ProductAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServiceWeb.ProductAPI.Model.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+        internal List<string> _errors;
+
+        //--Ao acessar os erros o programador só vai poder ler e não manipular os dados ex: _erros.Exemplo--
+        //public IReadOnlyCollection<string> Erros => _errors;
+        //public abstract bool Validate();
+    }
+}
